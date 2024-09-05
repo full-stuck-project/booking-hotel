e768247cec5f8dc1904ae00f942fd7413a27c458
SharonShak <sharonshakuri1@gmail.com>
2024-09-05 09:47:31 +0300

    hotelpage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,11 +10,12 @@ import { SignIn } from "./components/SignIn/SignIn";
 import { SearchFilter } from "./components/Fillters/SearchFillter/SearchFillter";
 import { Footer } from "./components/Footer/Footer";
 import NavBar from "./components/NavBar/NavBar";
+import { HotelPage } from "./user/pages/HotelPage/HotelPage";
 
 function App() {
   return (
     <div>
-    <NavBar/>
+      <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/signin" element={<SignIn />} />
@@ -22,6 +23,7 @@ function App() {
         <Route path="/pricefilter" element={<PriceFillter />} />
         <Route path="/filters" element={<Fillters />} />
         <Route path="/ratingfilter" element={<RatingFillter />} />
+        <Route path="/hotelpage" element={<HotelPage />} />
         {/* <Route path="/footer" element={<Footer />} /> */}
       </Routes>
     </div>
--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -99,130 +99,132 @@ const NavBar = () => {
   const toggleLanguageDropdown = () => setIsLanguageOpen(!isLanguageOpen);
 
   return (
-    <nav className="flex items-center justify-between bg-black/20 shadow-black shadow-sm text-6b6a68 h-16 w-full px-6 relative">
-      <div className="flex items-center">
-        {/* <img src={logo} alt="Logo" className="w-20 h-20 mr-4" /> */}
-      </div>
-      <div className="flex-1">
-        <ul className="flex justify-center space-x-8">
-          <li>
-            <a href="#" className="hover:text-white">
-              Home
-            </a>
-          </li>
-          <li>
-            <a href="#" className="hover:text-white">
-              About Us
-            </a>
-          </li>
-          <li>
-            <a href="#" className="hover:text-white">
-              Services
-            </a>
-          </li>
-          <li>
-            <a href="#" className="hover:text-white">
-              Blogs
-            </a>
-          </li>
-        </ul>
-      </div>
-      <div className="flex items-center space-x-6">
-        {/* Language Dropdown */}
-        <div className="relative">
-          <button
-            onClick={toggleLanguageDropdown}
-            className="hover:text-gray-400"
-          >
-            {/* <img src={globe} alt="Globe" className="h-7" /> */}
-          </button>
-          {isLanguageOpen && (
-            <div className="absolute right-0 mt-2 w-48 bg-white border border-gray-200 rounded-lg shadow-lg z-20">
-              <ul>
-                <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">
-                  English
-                </li>
-                <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">
-                  Hebrew
-                </li>
-                <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">
-                  Russian
-                </li>
-              </ul>
-            </div>
-          )}
+    <div className={`${isDarkMode ? "dark" : ""}`}>
+      <nav className="flex div items-center justify-between shadow-black shadow-sm text-6b6a68 h-16 w-full px-6 relative">
+        <div className="flex items-center">
+          {/* <img src={logo} alt="Logo" className="w-20 h-20 mr-4" /> */}
         </div>
-
-        {/* Notification Bell */}
-        <div className="relative">
-          <button
-            onClick={toggleNotificationDropdown}
-            className="hover:text-gray-400"
-          >
-            {/* <img src={bell} alt="Notification Bell" className="h-7" /> */}
-          </button>
-          {isNotificationOpen && (
-            <div className="absolute right-0 mt-2 w-48 bg-white border border-gray-200 rounded-lg shadow-lg z-20">
-              <ul>
-                <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">
-                  Notification 1
-                </li>
-                <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">
-                  Notification 2
-                </li>
-                <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">
-                  Notification 3
-                </li>
-              </ul>
-            </div>
-          )}
+        <div className="flex-1">
+          <ul className="flex justify-center space-x-8">
+            <li>
+              <a href="#" className="href">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="#" className="href">
+                About Us
+              </a>
+            </li>
+            <li>
+              <a href="#" className="href">
+                Services
+              </a>
+            </li>
+            <li>
+              <a href="#" className="href">
+                Blogs
+              </a>
+            </li>
+          </ul>
         </div>
+        <div className="flex items-center space-x-6">
+          {/* Language Dropdown */}
+          <div className="relative">
+            <button
+              onClick={toggleLanguageDropdown}
+              className="hover:text-gray-400"
+            >
+              {/* <img src={globe} alt="Globe" className="h-7" /> */}
+            </button>
+            {isLanguageOpen && (
+              <div className="absolute right-0 mt-2 w-48 bg-white border border-gray-200 rounded-lg shadow-lg z-20">
+                <ul>
+                  <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">
+                    English
+                  </li>
+                  <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">
+                    Hebrew
+                  </li>
+                  <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">
+                    Russian
+                  </li>
+                </ul>
+              </div>
+            )}
+          </div>
 
-        {/* Dark Mode Toggle */}
-        <div className="flex items-center space-x-2">
-          <button
-            type="button"
-            onClick={toggleDarkMode}
-            className={`w-10 h-6 flex items-center rounded-full ${
-              isDarkMode ? "bg-gray-600" : "bg-gray-300"
-            }`}
-          >
-            <div
-              className={`w-6 h-6 rounded-full transform transition-transform duration-300 ${
-                isDarkMode ? "bg-yellow-300 translate-x-4" : "bg-yellow-500"
+          {/* Notification Bell */}
+          <div className="relative">
+            <button
+              onClick={toggleNotificationDropdown}
+              className="hover:text-gray-400"
+            >
+              {/* <img src={bell} alt="Notification Bell" className="h-7" /> */}
+            </button>
+            {isNotificationOpen && (
+              <div className="absolute right-0 mt-2 w-48 bg-white border border-gray-200 rounded-lg shadow-lg z-20">
+                <ul>
+                  <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">
+                    Notification 1
+                  </li>
+                  <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">
+                    Notification 2
+                  </li>
+                  <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">
+                    Notification 3
+                  </li>
+                </ul>
+              </div>
+            )}
+          </div>
+
+          {/* Dark Mode Toggle */}
+          <div className="flex items-center space-x-2">
+            <button
+              type="button"
+              onClick={toggleDarkMode}
+              className={`w-10 h-6 flex items-center rounded-full ${
+                isDarkMode ? "bg-gray-600" : "bg-gray-300"
               }`}
-            ></div>
-          </button>
-        </div>
+            >
+              <div
+                className={`w-6 h-6 rounded-full transform transition-transform duration-300 ${
+                  isDarkMode ? "bg-yellow-300 translate-x-4" : "bg-yellow-500"
+                }`}
+              ></div>
+            </button>
+          </div>
 
-        {/* Profile Dropdown */}
-        <div className="relative">
-          <button
-            onClick={toggleProfileDropdown}
-            className="hover:text-gray-200"
-          >
-            <div className="h-10 w-10 bg-gray-100 rounded-full border-2 border-black flex items-center justify-center">
-              {/* <img src={profile} alt="Profile" className="h-7 rounded-full" /> */}
-            </div>
-          </button>
-          {isProfileOpen && (
-            <div className="absolute right-0 mt-2 w-48 bg-white border border-gray-200 rounded-lg shadow-lg z-20">
-              <ul>
-                <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">
-                  Profile
-                </li>
-                <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">
-                  Settings
-                </li>
-                <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">
-                  Logout
-                </li>
-              </ul>
-            </div>
-          )}
+          {/* Profile Dropdown */}
+          <div className="relative">
+            <button
+              onClick={toggleProfileDropdown}
+              className="hover:text-gray-200"
+            >
+              <div className="h-10 w-10 bg-gray-100 rounded-full border-2 border-black flex items-center justify-center">
+                {/* <img src={profile} alt="Profile" className="h-7 rounded-full" /> */}
+              </div>
+            </button>
+            {isProfileOpen && (
+              <div className="div absolute right-0 mt-2 w-48 rounded-lg shadow-lg z-20">
+                <ul>
+                  <li className="href px-4 py-2  cursor-pointer">
+                    Profile
+                  </li>
+                  <li className="href px-4 py-2  cursor-pointer">
+                    Settings
+                  </li>
+                  <li className="href px-4 py-2  cursor-pointer">
+                    Logout
+                  </li>
+                </ul>
+              </div>
+            )}
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </div>
   );
 };
 
